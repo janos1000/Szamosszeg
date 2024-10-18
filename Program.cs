c688ed830756e1467adc812de52501b5d83a237c
janos1000 <birinyij@kkszki.hu>
2024-10-18 11:01:07 +0200

    Számok bekérése is working.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main()
     {
-       
+            int positiveSum = 0;
+            int negativeSum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Kérem, adjon meg egy egész számot: ");
+                int number;
+
+                while (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Kérlek, érvényes egész számot adj meg!");
+                }
+
+
+                if (number > 0)
+                {
+                    positiveSum += number;
+                }
+                else if (number < 0)
+                {
+                    negativeSum += number;
+                }
+
+            }
+
+            Console.WriteLine($"A pozitív számok összeg: {positiveSum}");
+            Console.WriteLine($"A negatív számok összeg: {negativeSum}");
+        }
     }
-}
